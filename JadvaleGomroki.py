0181cada57ef9c449c659a45e31792283c8a0d67
ironcladgeek <ironcladgeek@hotmail.com>
2016-01-05 23:05:21 +0330

    added thousand separator to output

--- JadvaleGomroki.py
+++ JadvaleGomroki.py
@@ -1,33 +1,57 @@
 import math
 
-nerkhArz = input("Nerkhe Arz? ")
-amount = input("Arzeshe kala? ")
+def getNumInput(message=""):
+        """ getNumInput([message]) --> int
+        Keep getting input from user while its a coorect input and return the number. """
+	while True:
+		try:
+			_num = int(raw_input(message).strip())
+			break
+		except ValueError:
+			print "ERROR!! Lotfan meghdare addadi vared konid..."			
+	return _num
+
+def formatOutput(number):
+        """ formatOutput(number) --> str
+        Recieve a number and format it with thousand seprator then return it."""
+        lenght = len(str(number))
+        number = int(number)
+        if lenght>= 3:
+                return format(number, "03,d")
+        return number
+
+# Value
+nerkhArz = getNumInput("Nerkhe Arz? ")
+amount = getNumInput("Arzeshe kala? ")
 arzesh = nerkhArz * amount
 if math.modf(arzesh)[0] >= 0.5:
     arzesh = math.ceil(arzesh)
 else:
     arzesh = math.floor(arzesh)
 
+# Freight
 hasFreight = raw_input("Aya keraye haml darad?(Y/N) ")
-freight = 1.0;
 if hasFreight.lower() == 'y':
-    nerkheArzeFrieght = input("Nerkhe arze keraye haml? ")
-    freight = input("Keraye haml? ")
+    nerkheArzeFrieght = getNumInput("Nerkhe arze keraye haml? ")
+    freight = getNumInput("Keraye haml? ")
     freight = freight * nerkheArzeFrieght
     if math.modf(freight)[0] >= 0.5:
         freight = math.ceil(freight)
     else:
         freight = math.floor(freight)
+else:
+        freight = 1.0
 
-insurance = 0;
+# Insurance
 hasInsurance = raw_input("Aya kala bimeh nameh darad?(Y/N) ")
 if hasInsurance.lower() == 'y':
-    insurance = input("Bimeh>? ")
+    insurance = getNumInput("Bimeh? ")
 else:
     insurance = math.ceil((arzesh + freight) * 0.5 / 100)
 
+
 arzeshGomroki = arzesh + freight + insurance
-rate = input("Maakhaz? ")
+rate = getNumInput("Maakhaz gomroki? ")
 hoghoogh = arzeshGomroki * rate / 100
 if math.modf(hoghoogh)[0] >= 0.5:
     hoghoogh = math.ceil(hoghoogh)
@@ -56,17 +80,18 @@ total = hoghoogh + nimDarsad + maliat6Darsad + maliat3Darsad
 
 print ''
 print '#' * 35
-print 'Arzesh: %d' %arzesh
-print 'Keraye haml: %d' %freight
-print 'Bimeh: %d' %insurance
-print 'Jame CIF: %d' %arzeshGomroki
+print 'Arzesh: %s' %formatOutput(arzesh)
+print 'Keraye haml: %s' %formatOutput(freight)
+print 'Bimeh: %s' %formatOutput(insurance)
+print 'Jame CIF: %s' %formatOutput(arzeshGomroki)
 print ''
 print '*' * 35
-print 'Hoghoogh voroodi: %d ' % hoghoogh
-print 'Nimdarsad: %d ' % nimDarsad
-print 'Maliat 6 darsad: %d ' %maliat6Darsad
-print 'Maliat 3 darsad: %d ' %maliat3Darsad
+print 'Hoghoogh voroodi: %s ' %formatOutput(hoghoogh)
+print 'Nimdarsad: %s ' %formatOutput(nimDarsad)
+print 'Maliat 6 darsad: %s ' %formatOutput(maliat6Darsad)
+print 'Maliat 3 darsad: %s ' %formatOutput(maliat3Darsad)
 print '_' * 35
-print 'Jame kol: %d ' %total
+print 'Jame kol: %s RIAL' %formatOutput(total)
 
-input()
+raw_input("\nPress enter to exit...")
+exit()
