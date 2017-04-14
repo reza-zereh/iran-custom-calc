23086af9f5c5d886de7debe128eeb69d06d344ef
ironcladgeek <ironcladgeek@hotmail.com>
2017-04-15 00:06:33 +0430

    Installed & configured webpack and its loaders to work with Vue.js & css files.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <h1 class="title is-1">Hello</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './bootstrap.js';
+
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
